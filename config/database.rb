9964dc2f765bae6af877f06a1c5cf04959629988
GabrielGQA <gabrielgqa@gmail.com>
2019-06-05 22:45:38 -0300

    base

--- config/database.rb
+++ config/database.rb
@@ -19,4 +19,17 @@ configure :test do
       host: 'postgres'
     }
   end
+
+  configure :production do
+    db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/onebitbot_production')
+  
+    set :database, {
+      adapter:  'postgresql',
+      host:     db.host,
+      username: db.user,
+      password: db.password,
+      database: db.path[1..-1],
+      encoding: 'utf8'
+    }
+  end
   
